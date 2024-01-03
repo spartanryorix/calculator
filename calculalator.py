6ff4d7a68f33c3632bc6a6b00484e7d52d2579cf
spartanryorix <155487121+spartanryorix@users.noreply.github.com>
2024-01-03 20:21:11 +0300

    Add files via upload

---
+++ calculalator.py
@@ -0,0 +1,27 @@
+def calculator():
+    '''(int)->(int)
+    Returns a new float value when the user uses addition, subtraction, multiplication or division.
+    '''
+
+    print("Select an operation.")
+    print("1.ADD")
+    print("2.SUBTRACT")
+    print("3.MULTIPLY")
+    print("4.DIVIDE")
+
+    choice=input('Enter 1,2,3,4:')
+
+    if choice in ('1','2','3','4'):
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+    if choice=='1':
+        print(num1+num2)
+    elif choice=='2':
+        print(num1-num2)
+    elif choice=='3':
+        print(num1*num2)
+    elif choice=='4':
+        print(num1/num2)
+    
+        
