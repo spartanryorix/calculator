394a6c6336410c725815721f4fdb05d8886c1250
spartanryorix <155487121+spartanryorix@users.noreply.github.com>
2024-01-05 22:06:48 +0300

    Update calculalator.py

--- calculalator.py
+++ calculalator.py
@@ -9,20 +9,30 @@ def calculator():
     print("3.MULTIPLY")
     print("4.DIVIDE")
 
-    choice=input('Enter 1,2,3,4:')
+    while True:
 
-    if choice in ('1','2','3','4'):
-        num1 = float(input("Enter first number: "))
-        num2 = float(input("Enter second number: "))
+        choice=input('Enter 1,2,3,4:')
 
-    if choice=='1':
-        print(num1+num2)
-    elif choice=='2':
-        print(num1-num2)
-    elif choice=='3':
-        print(num1*num2)
-    elif choice=='4':
-        print(num1/num2)
+        if choice in ('1','2','3','4'):
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+
+        if choice=='1':
+            print(num1+num2)
+        elif choice=='2':
+            print(num1-num2)
+        elif choice=='3':
+            print(num1*num2)
+        elif choice=='4':
+            print(num1/num2)
+
+        next_calculation = input('Want to continue? (yes/no):')
+        if next_calculation == "no":
+          break
+        else:
+            None
+    
+
 
 if __name__ == "__main__":
     calculator()
