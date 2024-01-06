677aaa5cbbccd541d594d168f553c717d6386e8c
spartanryorix <155487121+spartanryorix@users.noreply.github.com>
2024-01-06 21:32:51 +0300

    Add files via upload

--- calculalator.py
+++ calculalator.py
@@ -9,18 +9,32 @@ def calculator():
     print("3.MULTIPLY")
     print("4.DIVIDE")
 
+    while True:
 
-    choice=input('Enter 1,2,3,4:')
+        choice=input('Enter 1,2,3,4:')
 
-    if choice in ('1','2','3','4'):
-        num1 = float(input("Enter first number: "))
-        num2 = float(input("Enter second number: "))
+        if choice in ('1','2','3','4'):
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
 
-    if choice=='1':
-        print(num1+num2)
-    elif choice=='2':
-        print(num1-num2)
-    elif choice=='3':
-        print(num1*num2)
-    elif choice=='4':
-        print(num1/num2)
+        if choice=='1':
+            print(num1+num2)
+        elif choice=='2':
+            print(num1-num2)
+        elif choice=='3':
+            print(num1*num2)
+        elif choice=='4':
+            print(num1/num2)
+
+        next_calculation = input('Press enter to continue operation, or n to cancel.:')
+        if next_calculation == "n":
+          break
+        else:
+            None
+    
+
+
+if __name__ == "__main__":
+    calculator()
+    
+        
